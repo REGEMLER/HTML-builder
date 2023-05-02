f1046897b098b40a9bb97929360f1cb63c745b8c
Yar Night <yaroslav239@gmail.com>
2023-05-02 20:01:56 +0300

    add task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const path = require('node:path'); 
+const pathToFolder = path.join(__dirname, '/files');
+const pathToNewFolder = path.join(__dirname, '/files-copy');
+const fs = require("fs");
+const fsp = require('node:fs/promises');
+const readdir = fsp.readdir
 
+async function copyFiles(){
+    try{
+        const files = await readdir(pathToFolder,{withFileTypes: true});
+        for (const file of files) {
+            const fileName = file.name;
+            const pathTocopeingFile = path.join(__dirname, '/files', `/${fileName}`);
+            const pathTocopiedFile = path.join(__dirname, '/files-copy', `/${fileName}`);
+            let readableStream = fs.createReadStream(pathTocopeingFile, "utf8");
+            let writeableStream = fs.createWriteStream(pathTocopiedFile);
+            readableStream.pipe(writeableStream);
+        } 
+    }
+    catch (error) {
+        console.error(error);
+    }
+} 
+
+fs.mkdir(pathToNewFolder, ()=>{
+    copyFiles(); 
+})
