53cb10cbbf42abccc516ddb518d96e7f41ee86dd
Yar Night <yaroslav239@gmail.com>
2023-05-05 21:41:18 +0300

    fix: fix errors in task 6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -7,20 +7,47 @@ const compPath = path.join(__dirname, '/components');
 const templateHTMLPath = path.join(__dirname, '/template.html');
 const fsp = require('node:fs/promises');
 const readdir = fsp.readdir;
-const readdir1 = fs.readdir;
+const readdirCB = fs.readdir;
 let componentsArr = []; 
 let stylesArr = []; 
-
 
 //replaise in indexHTmml 
 async function replaceTemplate(){
     fs.readFile(builderPath + "/index.html", "utf8", (error, text) => {
-        let [articles, footer, header] = componentsArr; 
-        let writeableStream = fs.createWriteStream(builderPath + "/index.html");
-        text = text.replace("{{header}}", header.data).replace("{{footer}}", footer.data).replace("{{articles}}", articles.data);
-        writeableStream.end(text);
+        if(componentsArr.length === 3) {
+            let [articles, footer, header] = componentsArr; 
+            let writeableStream = fs.createWriteStream(builderPath + "/index.html");
+            text = text.replace("{{header}}", header.data).replace("{{footer}}", footer.data).replace("{{articles}}", articles.data);
+            writeableStream.end(text);
+        } else {
+            let [about, articles, footer, header] = componentsArr; 
+            let writeableStream = fs.createWriteStream(builderPath + "/index.html");
+            text = text.replace("{{about}}", about.data).replace("{{header}}", header.data).replace("{{footer}}", footer.data).replace("{{articles}}", articles.data);
+            writeableStream.end(text);
+        }
     })
 }
+
+// async function replaceTemplate(){
+//     fs.readFile(builderPath + "/index.html", "utf8", (error, text) => {
+//         let component = componentsArr[0]; 
+//         let writeableStream = fs.createWriteStream(builderPath + "/index.html");
+//         text = text.replace(`{{${component.name}}}`, component.data); 
+//         writeableStream.end(text);
+//     })
+//     fs.readFile(builderPath + "/index.html", "utf8", (error, text) => {
+//         let component = componentsArr[1]; 
+//         let writeableStream = fs.createWriteStream(builderPath + "/index.html");
+//         text = text.replace(`{{${component.name}}}`, component.data); 
+//         writeableStream.end(text);
+//     })
+//     fs.readFile(builderPath + "/index.html", "utf8", (error, text) => {
+//         let component = componentsArr[2]; 
+//         let writeableStream = fs.createWriteStream(builderPath + "/index.html");
+//         text = text.replace(`{{${component.name}}}`, component.data); 
+//         writeableStream.end(text);
+//     })
+// }
 
 // set data from components to array; 
 async function readComponents(){
@@ -47,21 +74,27 @@ async function readComponents(){
 
 //copy styles 
 async function copyStyles(){
-    readdir1(stylesPath, (error, data) => {
-        if(error)  console.error(error);
-        data.forEach( (file, index, arr) => {
-            const pathToCurrentFile = path.join(stylesPath, `/${file}`);
-            fs.readFile(pathToCurrentFile, "utf8", (error, data) => {
-                if(error)  console.error(error);
-                stylesArr.push(data); 
-                if(stylesArr.length === arr.length){
-                    let writeableStream = fs.createWriteStream(builderPath + '/style.css');
-                    writeableStream.end(stylesArr.join("                          ")); 
-                }
-            })
-    
-        });
-        readComponents(); 
+    readdirCB(stylesPath, (error, data) => {
+        if(error) {
+            console.error(error);
+        }  else {
+            const styles = data.filter(file => path.extname(file) === ".css"); 
+            styles.forEach( (file, index, arr) => {
+                const pathToCurrentFile = path.join(stylesPath, `/${file}`);
+                fs.readFile(pathToCurrentFile, "utf8", (error, data) => {
+                    if(error) {
+                        console.error(error);
+                    } else {
+                        stylesArr.push(data); 
+                        if(stylesArr.length === arr.length){
+                            let writeableStream = fs.createWriteStream(builderPath + '/style.css');
+                            writeableStream.end(stylesArr.join(" ")); 
+                        }
+                    }
+                })
+            });
+            readComponents(); 
+        }
     })
 }
 
@@ -90,18 +123,16 @@ async function copyAssets(folder) {
     }
 } 
 
-
-
-
-
 //create index.html file, write data from template ti it and copy asses
 async function createIndex (data){
     fs.writeFile(builderPath + '/index.html', data, (error) => {
         if(error) console.error(error); 
         else {
-            fs.mkdir(builderPath + '/assets', ()=>{
-                copyAssets(""); 
-                copyStyles();
+            fs.rm(builderPath + '/assets', {recursive: true, force: true}, () => {
+                fs.mkdir(builderPath + '/assets', ()=>{
+                    copyAssets(""); 
+                    copyStyles();
+                })
             })
         } 
     })
@@ -117,8 +148,11 @@ async function readTemplate (){
     })
 }
 
-
 //Create builder folder
-fs.mkdir(builderPath, ()=>{
+async function htmlBuilder(){ 
+    await fsp.rm(builderPath, {recursive: true, force: true});
+    await fsp.mkdir(builderPath, {recursive: true});
     readTemplate(); 
-})
+}
+
+htmlBuilder(); 
